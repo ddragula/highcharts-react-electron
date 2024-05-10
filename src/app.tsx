d24dd8848b5ac2dc95db5615cc09ffceef63b828
Dawid Draguła <dawid.dragula@gmail.com>
2024-05-10 21:18:55 +0200

    Added react.

---
+++ src/app.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+import Chart from './components/Chart';
+
+const root = createRoot(document.body);
+root.render(<Chart />);
