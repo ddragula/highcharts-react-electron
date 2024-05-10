e6b21ba0a0330db139d2cc94f886c1e2709598c1
Dawid Draguła <dawid.dragula@gmail.com>
2024-05-10 21:27:23 +0200

    Added Highcharts.

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,5 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import HighchartsReact from 'highcharts-react-official';
+import Highcharts from 'highcharts';
 
 export default function Chart() {
-    return <h1>Chart Component</h1>
+    const [options] = useState<Highcharts.Options>({
+        title: {
+            text: 'My chart'
+        },
+        series: [{
+            type: 'line',
+            data: [1, 2, 3]
+        }]
+    });
+
+    return (
+        <HighchartsReact highcharts={Highcharts} options={options} />
+    );
 }
